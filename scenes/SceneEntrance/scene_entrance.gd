8a29554f3420b7c81ca8c29f398b1d5b47eafe96
LENOVO-JAKE-S\weile <weilerk02@gmail.com>
2025-06-06 18:45:18 -0400

    Testing commiting

--- scenes/SceneEntrance/scene_entrance.gd
+++ scenes/SceneEntrance/scene_entrance.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var next_scene: String
 @export var player_spawn_position: Vector2
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,6 +18,6 @@ func _on_body_entered(body: Node2D):
 	if body is Player: 
 		SceneManager.player_spawn_position = player_spawn_position
 		get_tree().change_scene_to_file.call_deferred(next_scene)
-
+		
 func _on_body_exited(body: Node2D):
 	pass # Replace with function body.
