eb94b05dec4b3f7b73853fe1bdbf7a599b194474
LENOVO-JAKE-S\weile <weilerk02@gmail.com>
2025-06-10 16:25:52 -0400

    added block movement logic

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 
 @export var move_speed: float = 100
+@export var push_strength: float = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,5 +31,15 @@ func _process(delta):
 		$AnimatedSprite2D.stop()
 		
 		
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	if collision: 
+		
+		var collider_node = collision.get_collider()
+		
+		if collider_node is RigidBody2D:
+			var collision_normal: Vector2 = collision.get_normal()
+			
+			collider_node.apply_central_force(-collision_normal * push_strength)
+		
 	move_and_slide()
-	
+	 
